de1666ba471dee2bdcad63a78d890d62ab726d5a
Hnzuonee <harvillajan@gmail.com>
2025-03-30 20:16:46 +0200

    redirect.js

--- mujviplink_com_final/netlify/functions/redirect.js
+++ mujviplink_com_final/netlify/functions/redirect.js
@@ -1,8 +1,6 @@
-exports.handler = async () => {
-  return {
-    statusCode: 302,
-    headers: {
-      Location: "https://onlyfans.com/kristynka.cengerova/c1",
-    },
-  };
+exports.handler = async function(event, context) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ url: "https://onlyfans.com/kristynka.cengerova/c1" }),
+    };
 };
